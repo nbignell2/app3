b22efd981700cf33428d6daebeead55f5c19e84e
nbignell2 <46579584+nbignell2@users.noreply.github.com>
2020-10-27 18:39:23 -0600

    Figuring out scopedmodel

--- app3_begin/lib/widget/DrinksCarousel.dart
+++ app3_begin/lib/widget/DrinksCarousel.dart
@@ -1,14 +1,42 @@
 import 'package:coffee_store_app/widget/DrinksCard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class DrinksCarousel extends StatefulWidget {
+  @override
+  DrinksCarouselState createState() {
+    return new DrinksCarouselState();
+  }
+}
 
 class DrinksCarouselState extends State<DrinksCarousel>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
-  Timer _carouselTimer;
-}
 
-class DrinksCarousel extends StatelessWidget {
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: coffeeTypes.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  void _changeImage({int delta, bool userInput = false}) {
+    var newTabIndex = _tabController.index + delta;
+    if (newTabIndex >= coffeeTypes.length) {
+      newTabIndex = 0;
+    } else if (newTabIndex < 0) {
+      newTabIndex = coffeeTypes.length - 1;
+    }
+    _tabController.animateTo(newTabIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -21,13 +49,59 @@ class DrinksCarousel extends StatelessWidget {
             color: Colors.black,
           ),
         ),
-        child: DrinksCard(
-          drinkType: mainTypes[0],
+        child: Stack(
+          children: <Widget>[
+            ScopedModelDescendant<MyModel>(
+              rebuildOnChange: false,
+              builder: (context, child, myModel) {
+                return TabBarView(
+              controller: _tabController,
+              children: mainTypes.map((drinkType) {
+                return DrinksCard(
+                  drinkType: drinkType,
+                );
+              }).toList(),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TabPageSelector(
+                  controller: _tabController,
+                  color: Colors.white,
+                  indicatorSize: 20.0,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  size: 36.0,
+                ),
+                onPressed: () {
+                  _changeImage(delta: -1, userInput: true);
+                },
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                  size: 36.0,
+                ),
+                onPressed: () {
+                  _changeImage(delta: 1, userInput: true);
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-
-  @override
-  _DrinksCarouselState createState() => _DrinksCarouselState();
 }
